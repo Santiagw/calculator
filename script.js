6f9c06a9091a8252f45dc6be5571caadef06d47e
santiagw <ottawa104421@gmail.com>
2025-05-27 16:34:07 -0500

    fix: call the operate function on operation button press

---
+++ script.js
@@ -0,0 +1,85 @@
+const calculatorContainer = document.querySelector("#calculator")
+
+function add(n1, n2) { return n1 + n2 }
+function substract(n1, n2) { return n1 - n2 }
+function multiply(n1, n2) { return n1 * n2 }
+function divide(n1, n2) { return n1 / n2 }
+
+let n1 = 0
+let n2 = 0
+let operator = ""
+
+function operate(operator, n1, n2) {
+    if (operator == "+") {
+        return add(n1, n2)
+    }
+    if (operator == "-") {
+        return substract(n1, n2)
+    } if (operator == "*") {
+        return multiply(n1, n2)
+    } if (operator == "/") {
+        return divide(n1, n2)
+    }
+}
+
+function updateDisplay() {
+    operationDisplay.innerHTML = `<p>${n1} <em>${operator}</em> ${n2}</p>`;
+}
+
+const addBtn = document.createElement("button");
+const subtractBtn = document.createElement("button");
+const multiplyBtn = document.createElement("button");
+const divideBtn = document.createElement("button");
+const clearBtn = document.createElement("button");
+
+addBtn.textContent = "+";
+subtractBtn.textContent = "-";
+multiplyBtn.textContent = "x";
+divideBtn.textContent = "/";
+clearBtn.textContent = "C";
+
+const equalsBtn = document.createElement("button");
+equalsBtn.textContent = "=";
+
+const operationDisplay = document.createElement("div");
+
+const createNumberButton = (displayNumber) => {
+    const number = document.createElement("button");
+    number.textContent = displayNumber;
+    number.addEventListener("click", () => {
+        if(n1==0){
+        n1 = displayNumber
+        }else{
+            n2 = displayNumber
+        }
+        updateDisplay()
+    })
+    return number;
+};
+
+addBtn.addEventListener("click", () => {
+    if(n1 != 0 && n2 != 0){
+        operator = "+"
+        updateDisplay()
+    }
+})
+
+calculatorContainer.append(
+    operationDisplay,
+    createNumberButton("1"),
+    createNumberButton("2"),
+    createNumberButton("3"),
+    addBtn,
+    createNumberButton("4"),
+    createNumberButton("5"),
+    createNumberButton("6"),
+    subtractBtn,
+    createNumberButton("7"),
+    createNumberButton("8"),
+    createNumberButton("9"),
+    multiplyBtn,
+    //   clearBtn,
+    createNumberButton("0"),
+    equalsBtn,
+    divideBtn
+);
